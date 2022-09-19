234daedc4d972daccafa5bd18e7c88f804b5936e
Marcin Lech <arcinech@gmail.com>
2022-09-19 22:51:07 +0200

    Add: repositories

---
+++ src/repository/users-mock-repository.ts
@@ -0,0 +1,48 @@
+import { UsersRepository } from './users-repository.interface';
+import { User } from '../interfaces/user.interface';
+import shortid from 'shortid';
+
+export class UsersMockRepository implements UsersRepository {
+  private users: Array<User> = [];
+
+  addItem(item: User): User {
+    item.id = shortid.generate();
+    this.users.push(item);
+    return item;
+  }
+
+  updateItem(id: string, item: User): User {
+    this.users = this.users.map(i => {
+      if (i.id === id) {
+        return {
+          ...item,
+          id: i.id,
+          updatedAt: new Date(),
+        };
+      }
+
+      return i;
+    });
+
+    return this.getElementById(id);
+  }
+
+  deleteItem(id: string): boolean {
+    this.users = this.users.filter(i => i.id !== id);
+    return !this.getElementById(id);
+  }
+
+  getElementById(id: string): User {
+    return this.users.find(i => i.id === id);
+  }
+
+  getAllItems(): Array<User> {
+    return this.users;
+  }
+
+  findUserByFirstName(firstname: string): User {
+    return this.users.find(i => i.firstname === firstname);
+  }
+}
+
+export default UsersMockRepository;
